3311ce7e2098f76524196e505358c1b0e4d2318d
SafwanKS <safwansalim340@gmail.com>
2025-07-15 23:38:30 +0530

    changes

--- frontend/src/firebase.js
+++ frontend/src/firebase.js
@@ -3,13 +3,13 @@ import {getAuth} from 'firebase/auth'
 import { getAnalytics } from "firebase/analytics";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyB4HIdf3-LXbKuBOtO1oD8tbZ7wBDzmZrs",
-  authDomain: "que--ai.firebaseapp.com",
-  projectId: "que--ai",
-  storageBucket: "que--ai.firebasestorage.app",
-  messagingSenderId: "832975211812",
-  appId: "1:832975211812:web:281fb623ec11da4c5237b1",
-  measurementId: "G-WSPEYXLLYM"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 
